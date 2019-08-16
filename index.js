bd040496ef90526cc80a5a75acc597d4e0fc0714
Francisco Reynolds <freynolds@gmail.com>
2019-08-16 11:56:48 -0300

    Changes: - Remove express - Remove scrap-it - Use ambito as references - Add axios - Add initial value on restart - Use PM2 to generate uptime

--- index.js
+++ index.js
@@ -4,37 +4,13 @@ const slack = new SlackWebhook(process.env.SLACK_WEBHOOK);
 let currentRate = 38.0;
 const tolerance = 0.02;
 const interval = 300000; // 5 minutes
-const express = require('express');
-const bodyParser = require('body-parser');
-const app = express();
-const port = process.env.PORT || 8080;
-const scrapeIt = require("scrape-it")
+const axios = require('axios');
 
-//Here we are configuring express to use body-parser as middle-ware.
-app.use(bodyParser.urlencoded({
-  extended: false
-}));
-app.use(bodyParser.json());
-
-// set the view engine to ejs
-app.set('view engine', 'ejs');
-
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
-
-app.listen(port, function () {
-  console.log('Our app is running on http://localhost:' + port);
-});
-
-app.post('/price', function (request, response) {
-  response.end(`1 USD = ${currentRate} ARS`);
-});
-
-getDiff = (rate) => {
+const getDiff = (rate) => {
   return (rate / currentRate) - 1;
 }
 
-getIcon = (diff) => {
+const getIcon = (diff) => {
   return diff < 0 ?
     '⬇️' :
     diff === 0 ?
@@ -42,17 +18,13 @@ getIcon = (diff) => {
     '⬆️';
 }
 
-sendToSlackChannel = (msg) => {
+const sendToSlackChannel = (msg) => {
   if (process.env.NODE_ENV === 'production') {
     slack.send(msg);
   }
 }
 
-app.get('/', (req, res) => {
-  sendToSlackChannel(currentRate);
-});
-
-updateRate = (rate) => {
+const updateRate = (rate) => {
   const diff = getDiff(rate);
   if (Math.abs(diff) >= tolerance) {
     const diff = getDiff(rate);
@@ -64,17 +36,24 @@ updateRate = (rate) => {
   }
 }
 
-getRate = () => {
-  scrapeIt("http://www.dolarhoy.com/", {
-    rate: "body > div > div > div > div.col-md-8 > div:nth-child(2) > div.col-md-6.venta > h4 .pull-right"
-  }).then(({
-    data
-  }) => {
-    const rate = data.rate.substring(2).replace(',', '.');
-    updateRate(Number(rate));
+const getRate = () => 
+  axios({
+    url: 'https://mercados.ambito.com/dolar/oficial/variacion',
+    method: 'get'
+  }).then(response => {
+    const rate = +response.data.venta.replace(',', '.')
+    return rate
+  })
+
+const setInitialRate = () => {
+  getRate().then(rate => {
+    console.log("inital rate", rate)
+    currentRate = rate
   })
 }
 
+setInitialRate()
+
 setInterval(() => {
-  getRate();
+  getRate().then(updateRate)
 }, interval);
